296acc68d095986717300b04f1755e6ba1868587
Robert Loo <rloolee89@gmail.com>
2022-10-20 10:59:31 -0400

    Start coding challenge of the day

---
+++ PartyOfCouples/PartyofCouples.js
@@ -0,0 +1,5 @@
+// Coding Challenge
+
+// In a party of N people, each person is denoted by an integer. Couples are represented by the same number. Find out the only single person in the party of couples.
+
+
