078ec8ef6c3aa68b0850d179852613a0a59aef5a
Robert Loo <rloolee89@gmail.com>
2022-10-24 19:08:44 -0400

    Completed 2nd coding challenge

--- Add/Add.ts
+++ Add/Add.ts
@@ -9,3 +9,18 @@ function sumOfNumbers (param1: number, param2: number) : number {
 }
 
 console.log (sumOfNumbers(1,2));
+
+
+
+// Write a function that returns the sum of all numbers regardless of # of params.
+// Learning take away: spread operators.
+
+
+function sumOfAllNums(...nums: number[]): number {
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++)
+    sum += nums[i];
+    return sum;
+}
+
+console.log(sumOfAllNums(1,2,3,4,10))
