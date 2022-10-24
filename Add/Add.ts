2626fc0ff0d1bfa4839d5b892af01e7d93b9eaf7
Robert Loo <rloolee89@gmail.com>
2022-10-24 18:44:10 -0400

    Solve first Challenge Question of the Day + started applying typescript

---
+++ Add/Add.ts
@@ -0,0 +1,11 @@
+
+//  Write a function that returns the sum of two numbers.
+
+//  Example For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3.
+
+
+function sumOfNumbers (param1: number, param2: number) : number {
+    return param1 + param2
+}
+
+console.log (sumOfNumbers(1,2));
---
+++ Python/python.py
@@ -0,0 +1,21 @@
+
+
+def function_name(age):
+    # age = int(input("Give me your age"))
+    print(str(age))
+
+age = int(input("Give me your age"))
+function_name(age)
+
+
+# true True
+
+# def function_name(param1, parm2, parm3...)
+# str function
+# coding challenges already include
+# casting = data type conversion
+# integer method
+# Strings = "Hello World" 
+# Boolean make sure to use Uppercase e.g(True // False)
+# conditions management
+# loops (e.g for loops)
