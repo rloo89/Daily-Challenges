82235064b13cbd58cc3f5c6ae34d4e591bde2ed0
Robert Loo <rloolee89@gmail.com>
2022-11-02 12:25:52 -0400

    Finish coding challenge

---
+++ alternatingSums/alternatingSums.js
@@ -0,0 +1,20 @@
+function alternatingSums (newArray) {
+    let weightOne = 0;
+    let weightTwo  = 0;
+
+    newArray.forEach((element, index) =>{
+        if(index % 2 === 0) {  
+            weightOne += element;
+        } else {
+            weightTwo += element;
+        }
+        
+    })
+
+    return [weightOne,weightTwo]
+}
+
+
+console.log(alternatingSums([50, 60, 60, 45, 70]));
+
+//    if(index % 2 === 0) thi is used as an approach to get into two groups. or  (i % 3)
---
+++ alternatingSums/alternatingSums.ts
@@ -0,0 +1,9 @@
+// export function alternatingSums(a: number[]): number[] {
+
+//     // 
+
+
+
+// }
+
+// console.log(alternatingSums([50, 60, 60, 45, 70]))
