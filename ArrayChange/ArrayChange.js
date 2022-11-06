b1a93c14a798a3b235cb0831205ad045e5047dbb
Robert Loo <rloolee89@gmail.com>
2022-11-05 23:57:33 -0400

    Array Change Coding Challenge

---
+++ ArrayChange/ArrayChange.js
@@ -0,0 +1,2 @@
+// You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
