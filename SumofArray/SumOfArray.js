4979b9be10c36952ac8b7bf7190dd889644e534b
Robert Loo <rloolee89@gmail.com>
2022-12-14 12:02:02 -0500

    Finish coding problem

---
+++ SumofArray/SumOfArray.js
@@ -0,0 +1,19 @@
+// Given an array of N integers. 
+// Your task is to print the sum of all of the integers.
+
+function sumOfArray(inputArray, number){
+
+    let numSum = 0;
+
+    for (let i = 0; i < inputArray.length; i++){
+        // numSum += inputArray[i]
+
+        numSum = numSum + inputArray[i]
+    }
+
+    return numSum
+}
+
+console.log(sumOfArray([1,2,3,4,5,6]));
+
+//  console.log the function means calling the function
